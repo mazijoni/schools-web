c3adb091412ea7212da37ec776db3f8c1a7d5291
Maze_joni <132272609+mazijoni@users.noreply.github.com>
2025-10-30 15:31:44 +0100

    .

--- school-finder.js
+++ school-finder.js
@@ -415,12 +415,14 @@ function renderSchools(schools) {
             <div class="school-details">
               ${school.website ? `
                 <div class="school-detail">
-                  🌐 <a href="${escapeHtml(school.website)}" target="_blank" rel="noopener">${escapeHtml(school.website)}</a>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 -960 960 960" width="17px" fill="#d30aac"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>
+                  <a href="${escapeHtml(school.website)}" target="_blank" rel="noopener">${escapeHtml(school.website)}</a>
                 </div>
               ` : ''}
               ${school.principal ? `
                 <div class="school-detail">
-                  👤 ${escapeHtml(school.principal)}
+                  <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 -960 960 960" width="17px" fill="#000000"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
+                  ${escapeHtml(school.principal)}
                 </div>
               ` : ''}
             </div>
